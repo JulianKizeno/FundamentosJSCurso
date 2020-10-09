0fbd504f93b81ae7e8fd0249c71c09e101b629d8
Julian Kizeno <julianquiceno20@gmail.com>
2020-10-10 00:24:08 +0200

    Reto ultima letra del nombre del usuario, termindo

--- clase2.js
+++ clase2.js
@@ -9,3 +9,10 @@ var cantidadDeLetrasDelNombre = nombre.length
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
 
 var str = nombre.substr(1, 2)
+
+// Reto ---
+
+let nombreUsuario = prompt('introduce tu nombre')
+let numeroLetrasNombre = nombreUsuario.length
+let ultimaLetraNombre = nombreUsuario.charAt(nombre.length-1)
+let mesajeParaElUsuario = alert(`La ultima letra de tu nombre es ${ultimaLetraNombre}`)
